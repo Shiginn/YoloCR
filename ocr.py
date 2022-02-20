742d093a28a4277895c0822e5e8c2273af83409f
Shigin <shigin.contact@gmail.com>
2022-02-20 14:23:01 +0100

    made linters and mypy less angry

--- ocr.py
+++ ocr.py
@@ -13,16 +13,17 @@ from typing import Callable, Tuple, Optional, List
 
 core = vs.core
 
-class OCR():
 
+class OCR():
+    """OCR Class"""
     def __init__(
         self,
-        
+
         clip_hardsub: vs.VideoNode,
-        
+
         coords: Tuple[int, int, int],
         coords_alt: Optional[Tuple[int, int, int]] = None,
-        
+
         thr_in: int = 220,
         thr_out: int = 70,
 
@@ -35,26 +36,26 @@ class OCR():
 
         :param coords:              Postion of the bottom detection box (width, height, vertical margin from the bottom).
 
-        :param coords_alt:          Postion of the top detection box (width, height, vertical margin from the top). 
+        :param coords_alt:          Postion of the top detection box (width, height, vertical margin from the top).
                                     Will increase processing time.
 
-        :param thr_in:              Binarization threshold of the subtitle inline. 
+        :param thr_in:              Binarization threshold of the subtitle inline.
                                     Higher means less errors but text might not be detected.
                                     Should not be higher than subtitle text luminosity.
                                     Defaults to 220 and ranges from 0 to 255 (will be scale if clip is not 8-bits)
 
-        :param thr_out:             Binarization threshold of the subtitle outline. 
+        :param thr_out:             Binarization threshold of the subtitle outline.
                                     Lower means more errors will be removed but text might be detected as error.
                                     Should not be lower than subtitle outline luminosity.
                                     Defaults to 70 and ranges from 0 to 255 (will be scale if clip is not 8-bits)
 
-        :param thr_sc_offset:       Offset the threshold of the subtitle timing detection. 
+        :param thr_sc_offset:       Offset the threshold of the subtitle timing detection.
                                     This threshold is determined based on detection box size and can be offset with this.
                                     Lower means more subtitles will be detected but might cause false positive.
                                     Defaults to 0.0035 when detection box is 1500x200 and go down as detection box size increases.
                                     Threshold is between 0 and 1.
 
-        :param rect_size:           Size of the rectangle used to detect cleaning errors. 
+        :param rect_size:           Size of the rectangle used to detect cleaning errors.
                                     Higher means more errors will be removed but might detect text as error.
                                     Defaults to 8.
         """
@@ -65,10 +66,10 @@ class OCR():
 
         if self.clip.format.color_family not in [vs.GRAY, vs.YUV]:
             raise ValueError("Input clip must be GRAY or YUV.")
-        
+
         if self.clip.format.num_planes > 1:
-            self.clip, *_ = core.std.SplitPlanes(self.clip)
-        
+            self.clip, *_ = core.std.SplitPlanes(self.clip)  # type: ignore
+
         if not (isinstance(thr_in, int) or isinstance(thr_out, int)):
             raise ValueError("Binarization threshold must be integers.")
 
@@ -94,13 +95,13 @@ class OCR():
 
     def write_subs(self, lang: str) -> None:
         """Write the ASS file from the extracted frames
-        
+
         :param lang:        Language of the input subtitles.
         """
 
         if lang not in pytesseract.get_languages():
             raise ValueError(f"Language '{lang}' is not installed")
-        
+
         try:
             os.mkdir("tesseract_results")
         except FileExistsError:
@@ -109,9 +110,9 @@ class OCR():
         print("OCRing images...")
 
         file_to_process = os.listdir("filtered_images")
-        file_to_process = [file for file in file_to_process if file.endswith("_yolocr.png")]
+        file_to_process = [file for file in file_to_process if file.endswith("_ocr.png")]
 
-        with Pool(cpu_count()-1) as p:
+        with Pool(cpu_count()) as p:
             lines = p.map(
                 partial(self._ocr_image, lang=lang), file_to_process
             )
@@ -123,7 +124,7 @@ class OCR():
 
     def _cleaning(self, clip: vs.VideoNode) -> vs.VideoNode:
         """Prepares the clip for the OCR by removing everything that is not subtitles.
-        
+
         :param clip:        Clip to process.
 
         :returns:           Cleaned clip.
@@ -131,18 +132,20 @@ class OCR():
         bnz_in = core.std.Binarize(clip, self.thr_in)
         bnz_out = core.std.Binarize(clip, self.thr_out)
 
-        blank_clip = core.std.BlankClip(bnz_in,
+        blank_clip = core.std.BlankClip(
+            bnz_in,
             width=clip.width - self.rect_size * 2,
             height=clip.height - self.rect_size * 2,
             color=0
         )
 
-        rect = core.std.AddBorders(blank_clip,
+        rect = core.std.AddBorders(
+            blank_clip,
             left=self.rect_size,
             right=self.rect_size,
             top=self.rect_size,
             bottom=self.rect_size,
-            color = self._scale_values(255, clip.format.bits_per_sample)
+            color=self._scale_values(255, clip.format.bits_per_sample)
         )
 
         overlap = core.std.Expr([rect, bnz_out], "x y min")
@@ -161,7 +164,7 @@ class OCR():
     @staticmethod
     def _ocr_image(file: str, lang: str) -> str:
         """OCR a single image and returns the result in ASS format
-        
+
         :param file:        Path to the image to process (format must be : <start timestamp>_<end_timestamp><_alt (optional)>.ext).
         :param lang:        Language of the input text.
 
@@ -172,14 +175,14 @@ class OCR():
 
         txt = txt.replace("<em>", "{\\i1}") \
                  .replace("</em>", "{\\i0}")        # replace html italics with ASS italics
-        
+
         txt = "".join(
-            list(ET.fromstring(txt).itertext())     # extract text from xml
+            list(ET.fromstring(txt).itertext())     # extract text from xml
         ).replace("{\\i1} {\\i0}", "").strip()      # remove whitespaces before/after text
-        
+
         txt = html.unescape(txt)                    # unescape html caracters
         txt = re.sub(r"\n{1} +", " ", txt)          # convert 1 line break into whitespace
-        txt = re.sub(r" {2,}", r" \\N", txt)        # convert 2+ line break into one ASS linebreak
+        txt = re.sub(r" {2,}", r" \\N", txt)        # convert 2+ line break into one ASS linebreak
         txt = re.sub(                               # remove redundant italic tags
             r"{\\i0}(\W+| \\N){\\i1}",
             lambda x: x.group()[5:-5],
@@ -203,9 +206,9 @@ class OCR():
                 txt = txt[:1] + r"\an8" + txt[1:]
             else:
                 txt = r"{\an8}" + txt
-        
+
         start_ts, end_ts, *_ = file.split("_")
-        
+
         start_ts = start_ts.replace("-", ":")
         end_ts = end_ts.replace("-", ":")
 
@@ -225,7 +228,7 @@ class OCR():
                 scene_changes.insert(-1, n)
             return clip
 
-        thr_sc = 0.0035 * (300000/(clip.width * clip.height)) + self.thr_sc_offset
+        thr_sc = 0.0035 * (300000 / (clip.width * clip.height)) + self.thr_sc_offset
 
         ocr = core.std.FrameEval(
             clip,
@@ -234,10 +237,10 @@ class OCR():
         )
 
         print(f"Detecting subtitles in {'main' if not alt else 'alt'} clip")
-        self._output_to_devnull(ocr, True, lambda x, y : print("%d/%d [%.1f%%]" % (x, y, x/y * 100), end="\r"))
+        self._output_to_devnull(ocr, True, lambda x, y: print("%d/%d [%.1f%%]" % (x, y, x / y * 100), end="\r"))
 
         scene_changes = sorted(scene_changes)
-        txt_scenes = [(scene_changes[i], scene_changes[i+1]) for i in range(0, len(scene_changes)-1, 2)][1::2]
+        txt_scenes = [(scene_changes[i], scene_changes[i + 1]) for i in range(0, len(scene_changes) - 1, 2)][1::2]
 
         try:
             os.mkdir("filtered_images")
@@ -249,10 +252,10 @@ class OCR():
 
         for (start_f, end_f) in txt_scenes:
             # using frame number in path and converting to timestamp in _ocr_image somehow creates a memory leak
-            start_ts = Convert.f2assts(start_f, clip.fps_num/clip.fps_den).replace(":", "-")
-            end_ts = Convert.f2assts(end_f+1, clip.fps_num/clip.fps_den).replace(":", "-")
+            start_ts = Convert.f2assts(start_f, Fraction(clip.fps_num, clip.fps_den)).replace(":", "-")
+            end_ts = Convert.f2assts(end_f + 1, Fraction(clip.fps_num, clip.fps_den)).replace(":", "-")
 
-            path = f"{os.getcwd()}/filtered_images/{start_ts}_{end_ts}{'_alt' if alt else ''}_%01d_yolocr.png"
+            path = f"{os.getcwd()}/filtered_images/{start_ts}_{end_ts}{'_alt' if alt else ''}_%01d_ocr.png"
 
             if os.path.isfile(path):
                 os.remove(path)
@@ -261,22 +264,22 @@ class OCR():
                 imgformat="PNG",
                 filename=path
             )
-            
+
             self._output_to_devnull(frame, y4m=False)
 
 
     @property
     def preview_coords(self) -> vs.VideoNode:
         """Preview of the OCR zone(s)"""
-        base = self.clip.std.Lut(0, function=lambda x: int(x/2))
+        base = self.clip.std.Lut(0, function=lambda x: int(x / 2))
 
         preview = core.std.MaskedMerge(base, self.clip, self._zone_mask(self.coords))
         if self.coords_alt:
             preview = core.std.MaskedMerge(preview, self.clip, self._zone_mask(self.coords_alt))
-        
+
         return preview
 
-    
+
     @property
     def preview_crop(self) -> vs.VideoNode:
         """Preview of the cropped input"""
@@ -287,7 +290,7 @@ class OCR():
             top = self._crop(self.clip, self.coords_alt)
             bottom = self._crop(self.clip, self.coords)
 
-            diff = (int((top.width - bottom.width)/2), int((top.width - bottom.width)/2))
+            diff = (int((top.width - bottom.width) / 2), int((top.width - bottom.width) / 2))
 
             if top.width > bottom.width:
                 bottom = bottom.std.AddBorders(*diff)
@@ -305,14 +308,14 @@ class OCR():
 
     def _zone_mask(self, coords: Tuple[int, int, int, int]) -> vs.VideoNode:
         """Generates rectangular mask of the zone to OCR
-        
+
         :param coords:      Amount of pixel to crop for each side.
 
         :return:            Mask of the zone.
         """
-        
+
         left, right, top, bottom = coords
-        
+
         return self._crop(
             core.std.BlankClip(self.clip, color=[self._scale_values(255, self.clip.format.bits_per_sample)] * self.clip.format.num_planes),
             coords
@@ -322,7 +325,7 @@ class OCR():
     @staticmethod
     def _crop(clip: vs.VideoNode, coords: Tuple[int, int, int, int]) -> vs.VideoNode:
         """Crop a clip to the input coords
-        
+
         :param coords:      Amount of pixel to crop for each side.
 
         :return:            Cropped clip.
@@ -335,7 +338,7 @@ class OCR():
     @staticmethod
     def _convert_coords(clip: vs.VideoNode, coords: Tuple[int, int, int], alt: bool = False) -> Tuple[int, int, int, int]:
         """Convert OCR coords to std.Crop coords
-        
+
         :param coords:      coords of the zone to OCR
 
         :return:            std.Crop coords
@@ -343,17 +346,17 @@ class OCR():
 
         width, height, offset = coords
         return (
-            int((clip.width - width)/2),
-            int((clip.width - width)/2),
+            int((clip.width - width) / 2),
+            int((clip.width - width) / 2),
             int(clip.height - height - offset) if not alt else int(offset),
             int(offset) if not alt else (clip.height - height - offset)
         )
-    
+
 
     @staticmethod
     def _scale_values(n: int, bitdepth: int, src_bitdepth: int = 8) -> int:
         """Scale a value to the correct bitdepth
-        
+
         :param n:           value to scale
         :param bitdepth:    bitdepth to scale the value to
 
@@ -365,7 +368,7 @@ class OCR():
     @staticmethod
     def _output_to_devnull(clip: vs.VideoNode, y4m: bool, progress_update: Optional[Callable[[int, int], None]] = None) -> None:
         """Output a clip to devnull.
-        
+
         :param clip:                Clip to output.
 
         :param y4m:                 Output in y4m.
@@ -374,12 +377,12 @@ class OCR():
         """
         with open(os.devnull, "wb") as devnull:
             clip.output(devnull, y4m, progress_update)
-    
+
 
     @staticmethod
     def _get_sub_headers(width: int, height: int) -> str:
         """Generate ASS headers with custom resolution.
-        
+
         :param width:       Width of the source video.
         :param height:      Height of the source video.
 
